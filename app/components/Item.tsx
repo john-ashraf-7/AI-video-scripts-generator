4209c97e46502789e5956ec18d10a6485e55729e
john-ashraf-7 <johnashraf@aucegypt.edu>
2025-07-30 07:55:16 +0300

    added an item and a counter(generatebutton) components

---
+++ app/components/Item.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function Item({ onSelect }: { onSelect: (index: number) => void }) {
+    const [isActive, setIsActive] = useState(true);
+    
+    return(
+        <div className="w-64 h-80 p-4 bg-white shadow rounded-lg flex flex-col m-4">
+            <div className="flex-1 relative">
+                <img 
+                    src="https://digitalcollections.aucegypt.edu/iiif/2/p15795coll19:30192/full/730,/0/default.jpg" 
+                    alt="Item Image" 
+                    className="w-full h-full object-cover rounded"
+                />
+            </div>
+            <button className="mt-auto w-full bg-blue-600 text-white text-sm py-2 rounded hover:bg-blue-700 transition"
+            onClick={() => {
+                if (isActive) {
+                    onSelect(1); // Increment the selected item count
+                    setIsActive(false); // Disable further selection
+                }
+                else {
+                    alert("Item already selected");
+                }
+            }}>
+                Select Item
+            </button>
+        </div>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+import Item from './components/Item';
+import GenerateButton from './components/GenerateButton';
+
+import { useState } from 'react';
+
 export default function Home() {
+  const [selectedItemCount, setSelectedItemCount] = useState(0);
+
+  const IncrementSelectedItem = () => {
+    setSelectedItemCount(selectedItemCount + 1);
+  };
+
   return (
     <div>
       <h1>Welcome to the AI Video Script Generator</h1>
+      <div className="flex flex-wrap justify-center">
+        <Item onSelect={IncrementSelectedItem} />
+        <Item onSelect={IncrementSelectedItem} /> 
+        <Item onSelect={IncrementSelectedItem} />
+      </div>
+      <GenerateButton className="" itemscount={selectedItemCount} />
     </div>
   );
 }
